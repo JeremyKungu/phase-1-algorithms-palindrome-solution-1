d0057e335e0aa0c5eeb9ce5481dfd6d952b9a894
JeremyKungu <jeremikungu27@gmail.com>
2022-08-25 19:37:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -6,24 +6,16 @@ function reverse(word) {
 }
 
 function isPalindrome(word) {
-  // Write your algorithm here
+
   const reversedWord = reverse(word)
 
   return word === reversedWord;
 }
 isPalindrome("racecar")
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
